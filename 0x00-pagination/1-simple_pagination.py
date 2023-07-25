c15ee3cb3d3abf10bfa6c1733d4db3ed4b3013da
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-07-25 07:37:00 +0100

    added task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 """
 Task:
-    Copy index_range from the previous task and the following class into your code
-    Implement a method named get_page that takes two integer arguments page with
+    Copy index_range from the previous task and the following
+      class into your code
+    Implement a method named get_page that takes two integer
+      arguments page with
     default value 1 and page_size with default value 10.
-    You have to use this CSV file (same as the one presented at the top of the project)
+    You have to use this CSV file (same as the one presented at the
+      top of the project)
     Use assert to verify that both arguments are integers greater than 0.
-    Use index_range to find the correct indexes to paginate the dataset correctly and return
+    Use index_range to find the correct indexes to paginate the dataset
+      correctly and return
     the appropriate page of the dataset (i.e. the correct list of rows).
-    If the input arguments are out of range for the dataset, an empty list should be returned.
+    If the input arguments are out of range for the dataset, an empty
+      list should be returned.
 """
 
 import csv
